555e731a959df370e6fed66163d256aac16e1810
Saso Sabotin <sasos90@gmail.com>
2015-06-04 12:36:40 +0200

    Data controller created

---
+++ application/controllers/Data.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Data extends CI_Controller
+{
+    public function advertisements()
+    {
+        echo "advertisement list";die();
+    }
+}
