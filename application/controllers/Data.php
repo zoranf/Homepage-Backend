72730d6fe9c04e1676aa33e698705bb5a28a2df4
zoranf <zoran.felbar@gmail.com>
2015-06-04 13:04:21 +0200

    Adveretisment route name changed to ads

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Data extends CI_Controller
 {
-    public function advertisements()
+    public function ads()
     {
         // test response
         $array = [
