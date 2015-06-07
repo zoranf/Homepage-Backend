7be43a96ec2408b3e9b62629032c283dfff75aa8
olyar <zoran.felbar@gmail.com>
2015-06-07 12:17:02 +0200

    Created get, post, delete, enable and disable functions

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -1,33 +1,51 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Data extends MY_Controller
-{
-    public function getAds()
-    {
+class Data extends MY_Controller {
+
+    function __construct() {
+
+        parent::__construct();
+
+        // Load Advertisements_model
         $this->load->model("Advertisements_model");
+    }
+
+    // Returns list of advertisements
+    public function getAds() {
+
         $data = $this->Advertisements_model->getAdList();
 
         return $this->_returnAjax(true, $data);
     }
 
-    public function postAds()
-    {
-        print json_encode(["success" => true]);die();
+    // Add new advertisement
+    public function postAds($title, $picture, $enabled) {
+
+        $data = [
+            "title"     => $title,
+            "picture"   => $picture,
+            "enabled"   => $enabled
+        ];
+
+        $this->Advertisements_model->add($data);
     }
 
-    public function deleteAds()
-    {
-        print json_encode(["success" => true]);die();
+    // Delete spcific advertisement
+    public function deleteAds($id) {
+
+        $this->Advertisements_model->delete(array("id" => $id));
     }
 
-    public function enableAd()
-    {
-        print json_encode(["success" => true]);die();
+    // Enable / spcific ad
+    public function enableAd($id) {
+
+        $this->Advertisements_model->enable($id, 1);
     }
 
-    public function disableAd()
-    {
-        print json_encode(["success" => true]);die();
+    // Disable spcific ad
+    public function disableAd($id) {
+
+        $this->Advertisements_model->enable($id, 0);
     }
 }
