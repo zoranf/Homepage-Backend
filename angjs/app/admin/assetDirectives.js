05198f799610c30246c7a676a6f3ce1919f01b6d
Saso Sabotin <sasos90@gmail.com>
2015-07-23 21:55:52 +0200

    Combined with angular

---
+++ angjs/app/admin/assetDirectives.js
@@ -0,0 +1,10 @@
+kmtApp.directive('backgrImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backgrImg', function(value) {
+            var url = BACKEND_IP + "/upload/" + value;
+            element.css({
+                'background-image': 'url(' + url +')'
+            });
+        });
+    };
+});
