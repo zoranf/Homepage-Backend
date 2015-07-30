f411774773646c438606e6fcd78b09abbe322f97
Zoran Felbar <zoran.felbar@gmail.com>
2015-07-30 16:44:00 +0200

    added smooth scroll

--- angjs/app/main/sliderCtrl.js
+++ angjs/app/main/sliderCtrl.js
@@ -1,4 +1,4 @@
-app.controller("sliderCtrl", ["$scope", "sliderService", function($scope, sliderService, $location, $anchorScroll) {
+app.controller("sliderCtrl", ["$scope", "sliderService", function($scope, sliderService, $location) {
     // sliderCtrl controller
     $scope.sliderList = [];
 
@@ -20,9 +20,56 @@ app.controller("sliderCtrl", ["$scope", "sliderService", function($scope, slider
 }]);
 
 // Scroll to section
-app.controller('scrollToCtrl', function($scope, $location, $anchorScroll) {
+app.controller('scrollCtrl', function($scope, $location) {
     $scope.scrollTo = function(id) {
         $location.hash(id);
-        $anchorScroll();
+        smoothScroll(id);
     };
+
+    function smoothScroll(eID) {
+        var startY = currentYPosition();
+        var height = 84;
+        var stopY = elmYPosition(eID) - height;
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (startY === 0) stopY = stopY - height;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
+
+    function elmYPosition(id) {
+        var elm = document.getElementById(id);
+        var y = elm.offsetTop;
+        var node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+            node = node.offsetParent;
+            y += node.offsetTop;
+        } return y;
+    }
+
+    function currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+        return 0;
+    }
 });
